a715cafc2ffe6bf477584105cc0bd938edd919e5
Ibi Awesu <Ibi.Awesu@brandbank.online>
2025-09-05 09:35:39 +0100

    test scripts featuring footer

---
+++ src/test/scala/uk/gov/hmrc/api/service/FooterService.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.api.service
+
+import play.api.libs.ws.DefaultBodyWritables.writeableOf_String
+import play.api.libs.ws.StandaloneWSResponse
+import uk.gov.hmrc.api.conf.TestEnvironment
+import uk.gov.hmrc.apitestrunner.http.HttpClient
+
+import scala.concurrent.Await
+import scala.concurrent.duration.*
+
+class FooterService extends HttpClient {
+  val disa_returns_host: String = TestEnvironment.url("disa-returns")
+
+  def postMonthlyReturnsSubmission(
+    isaManagerReference: String,
+    returnId: String,
+    headers: Map[String, String],
+    ndString: String = ""
+  ): StandaloneWSResponse =
+    Await.result(
+      mkRequest(disa_returns_host + isaManagerReference + "/" + returnId)
+        .withHttpHeaders(headers.toSeq: _*)
+        .post(ndString),
+      10.seconds
+    )
+}
