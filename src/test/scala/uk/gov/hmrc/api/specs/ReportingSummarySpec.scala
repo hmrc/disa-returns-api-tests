23e4a3610865c2eda0718a917d123f6fb8c09ecb
Ibi Awesu <26194202+way2autotesting@users.noreply.github.com>
2025-11-11 14:24:29 +0000

    Results Endpoint

--- src/test/scala/uk/gov/hmrc/api/specs/ReportingSummarySpec.scala
+++ src/test/scala/uk/gov/hmrc/api/specs/ReportingSummarySpec.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.api.specs
 
 import com.typesafe.scalalogging.LazyLogging
 import org.scalactic.Prettifier.default
-import play.api.libs.json.Json
+import play.api.libs.json.{JsArray, Json}
 import play.api.libs.ws.StandaloneWSResponse
 import uk.gov.hmrc.api.utils.BaseSpec
 
@@ -53,7 +53,7 @@ class ReportingSummarySpec extends BaseSpec, LazyLogging {
     val json = Json.parse(receivedReportingResultsSummaryResponse.body)
 
     (json \ "returnResultsLocation").as[String] should include(
-      s"/monthly/$isaReference/$taxYear/$month/results?page=1"
+      s"/monthly/$isaReference/$taxYear/$month/results?page=0"
     )
     (json \ "totalRecords").as[Int]        shouldEqual totalRecords
     (json \ "numberOfPages").as[Int]            should be > 0
@@ -88,7 +88,7 @@ class ReportingSummarySpec extends BaseSpec, LazyLogging {
     val json = Json.parse(receivedReportingResultsSummaryResponse.body)
 
     (json \ "returnResultsLocation").as[String] should include(
-      s"/monthly/$isaReference/$taxYear/$month/results?page=1"
+      s"/monthly/$isaReference/$taxYear/$month/results?page=0"
     )
     (json \ "totalRecords").as[Int]        shouldEqual totalRecords.sum
     (json \ "numberOfPages").as[Int]            should be > 0
@@ -111,5 +111,60 @@ class ReportingSummarySpec extends BaseSpec, LazyLogging {
     (json \ "message").as[String] should include(
       s"No return found for $isaReference for $month $taxYear"
     )
+  }
+
+  Scenario(
+    s"4. Verify 'Results Endpoint' returns status code 200 OK after successful reconciliation declaration"
+  ) {
+    Given("A valid reconciliation report has been declared successfully via the declaration endpoint")
+    val isaReference                                  = generateRandomZReference()
+    val totalRecords                                  = Array(1, 2, 3)
+    val month                                         = "AUG"
+    val receivedSummaryResponse: StandaloneWSResponse =
+      reportingService.triggerReportReadyScenario(
+        isaReference,
+        taxYear,
+        month,
+        totalRecords,
+        validHeadersOnlyWithToken
+      )
+
+    Then("I got the status code 204 confirming the data was successfully stored")
+    receivedSummaryResponse.status shouldBe 204
+
+    When("I call the 'Reporting Results Endpoint' to retrieve the full report results from reconciliation")
+    val checkResponse                                 =
+      reportingService.getReportingResultsSummary(isaReference, taxYear, month, validHeadersOnlyWithToken)
+    println(s"Summary check status: ${checkResponse.status}, body: ${checkResponse.body}")
+    val receivedResultsResponse: StandaloneWSResponse =
+      reportingService.getReportingResults(
+        isaReference,
+        taxYear,
+        month,
+        page = 0,
+        validHeadersOnlyWithToken
+      )
+
+    Then("I should receive status code 200 OK")
+    receivedResultsResponse.status shouldBe 200
+
+    And("The response body should contain valid report data from reconciliation")
+    val json = Json.parse(receivedResultsResponse.body)
+    (json \ "currentPage").as[Int]   shouldEqual 0
+    (json \ "recordsInPage").as[Int]      should be > 0
+    (json \ "totalRecords").as[Int]       should be >= (json \ "recordsInPage").as[Int]
+    (json \ "totalRecords").as[Int]  shouldEqual totalRecords.sum
+    (json \ "totalNumberOfPages").as[Int] should be > 0
+
+    And("Each return result should include valid account information and reconciliation details")
+    val returnResults = (json \ "returnResults").as[JsArray]
+    returnResults.value.nonEmpty shouldBe true
+
+    val firstRecord = returnResults.value.head
+    (firstRecord \ "accountNumber").asOpt[String].isDefined           shouldBe true
+    (firstRecord \ "accountNumber").asOpt[String].nonEmpty            shouldBe true
+    (firstRecord \ "nino").asOpt[String].isDefined                    shouldBe true
+    (firstRecord \ "nino").asOpt[String].nonEmpty                     shouldBe true
+    (firstRecord \ "issueIdentified" \ "code").asOpt[String].nonEmpty shouldBe true
   }
 }
